5506c41d011c7ec7d4a8cf04e1382f8fbb35373f
gaofeng13 <gaofeng13@meituan.com>
2016-07-19 20:06:17 +0800

    save

--- app.js
+++ app.js
@@ -83,9 +83,8 @@ function delete_old(file) {
 
 function proceed(file) {
   var final_name = file.substr(0, file.length - 4) + '__'
-    + moment().subtract(1, durationLegend[INTERVAL_UNIT]).format(DATE_FORMAT) + '.log';
+    + moment().subtract(force ? 1 : 0, durationLegend[INTERVAL_UNIT]).format(DATE_FORMAT) + '.log';
 
-    /*
     if (processing_files[file]) {
       return;
     }
@@ -107,7 +106,6 @@ function proceed(file) {
           delete processing_files[file];
         });
     }
-    */
 
     //app-pid__2016-06-16-14-44
     var fileName = final_name.slice(final_name.lastIndexOf('/') + 1, final_name.length).replace('.log', '');
@@ -132,20 +130,8 @@ function proceed(file) {
             final_name = path + '/' + fileName + '__' + (versions[0] + 1) + '.log';
         }
 
-        backLogFile(file, final_name);
+        pipeNew(final_name);
     });
-
-    function backLogFile(file, final_name) {
-        fs.rename(file, final_name, function() {
-            pm2.reloadLogs(function(e, msg) {
-                console.log(e, msg);
-            });
-            console.log('"' + final_name + '" has been created');
-            if (RETAIN !== undefined) {
-                delete_old(file);
-            }
-        });
-    }
 }
 
 function proceed_file(file, force) {
@@ -157,7 +143,7 @@ function proceed_file(file, force) {
   var size = fs.statSync(file).size;
 
   if (size > 0 && (size >= SIZE_LIMIT || force)) {
-    proceed(file);
+    proceed(file, force);
   }
 }
 
